5be4aad6879013fadcda8b3bd0117b6eb411c461
vagrant <vagrant@localhost.localdomain>
2015-09-04 02:24:57 +0000

    tst de chef

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default["tomcat"]["base_version"] = 7
+default["tomcat"]["java_options"] = "-Xmx2048m -Djava.awt.headless=true"
+default["java"]["install_flavor"] = "oracle"
+default["java"]["jdk_version"] = 7
+default["java"]["accept_license_agreement"] = "yes"
+default["java"]["oracle"]["accept_oracle_download_terms"] = "yes"
